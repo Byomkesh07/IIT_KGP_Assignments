f7df8a0897bca7ac765b80e04fcfa341350419a0
Byomkesh <byomkeshbhattacharjee2000@gmail.com>
2024-07-26 22:21:33 +0530

    Using Command Line Urgument

--- Programming_Assignment_1.py
+++ Programming_Assignment_1.py
@@ -1,7 +1,7 @@
 import sys
 
 def frac(n: int) -> int:
-    
+
     if n == 0:
         return 1
     if n == 1:
@@ -9,8 +9,7 @@ def frac(n: int) -> int:
     else:
         return n * frac(n - 1)
     
-def series(ip: int) -> float:
-
+def series(ip: int) -> float:   
     total_sum = 0.0
     for i in range(1, ip + 1):
         factorial_i = frac(i)  # Calculate factorial of i using the frac function
@@ -23,14 +22,5 @@ def series(ip: int) -> float:
 
 ## DO NOT MODIFY THIS PART, Please maintain this structure
 if __name__ == "__main__":
-    print("sys.argv:", sys.argv)
-    simulated_args = ['compute_series.py', '5']
-    sys.argv = simulated_args
-    
-    try:
-        value = series(int(sys.argv[1]))
-        print(value)
-    except IndexError:
-        print("Error: Please provide an integer input.")
-    except ValueError:
-        print("Error: Please provide a valid integer input.")
+    value = series(int(sys.argv[1]))
+    print(value)
